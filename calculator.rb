1e5524cb1d2f249023c30f68d90b100e1ecfdd5c
lior Shine <shinelior@gmail.com>
2019-12-10 01:05:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -32,10 +32,12 @@ def difference(first_number,second_number)
        
        
 first_number=25
-  second_number=30 
+second_number=30 
+  
    sum = first_number+second_number
    difference=first_number-second_number
-    
+   product=first_number*second_number 
+   
 calculator(first_number,second_number)    
   
   
