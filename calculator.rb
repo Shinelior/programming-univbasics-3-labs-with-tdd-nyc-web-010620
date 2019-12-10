41d4db33ac4e1ca9a68e0afb73b9f45fb7d7f868
lior Shine <shinelior@gmail.com>
2019-12-10 01:06:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -37,7 +37,6 @@ second_number=30
    sum = first_number+second_number
    difference=first_number-second_number
    product=first_number*second_number 
-   
-calculator(first_number,second_number)    
-  
+   quotient=first_number/second_number
+
   
