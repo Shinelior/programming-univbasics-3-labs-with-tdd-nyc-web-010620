4f387575f6282a09ef034c37b5841766afa8a7e6
lior Shine <shinelior@gmail.com>
2019-12-10 00:59:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,40 @@
 # Add your variables here
+def calculator(first_number,second_number)
+  first_number=25
+  second_number=30
+  def sum (first_number,second_number)
+  sum=0  
+  sum = first_number+second_number
+  return sum
+  
+end
+
+def difference(first_number,second_number)
+    difference=0  
+    difference=first_number-second_number
+    return difference
+    
+  end  
+  
+  def product(first_number,second_number)
+      product=0 
+      product=first_number*second_number
+      return product
+      
+    end
+    
+  def quotient(first_number,second_number)
+       quotient=0
+       quotient=first_number/second_number
+       
+       return quotient
+       
+     end
+     
+   end
+       
+       
+      
+    
+  
+  
